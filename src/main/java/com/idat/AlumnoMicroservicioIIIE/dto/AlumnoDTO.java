bea47402f47825ff5ca484ecadadf3c3faca12e4
Eder Romero <ederromero32@gmail.com>
2022-11-22 22:48:40 -0500

    agregando DTO Alumno

--- src/main/java/com/idat/AlumnoMicroservicioIIIE/dto/AlumnoDTO.java
+++ src/main/java/com/idat/AlumnoMicroservicioIIIE/dto/AlumnoDTO.java
@@ -1,28 +1,13 @@
 package com.idat.AlumnoMicroservicioIIIE.dto;
 
+import lombok.Data;
+
+@Data
 public class AlumnoDTO {
 	
 	private Integer cod;
 	private String nom;
 	private String ape;
 	
-	public Integer getCod() {
-		return cod;
-	}
-	public void setCod(Integer cod) {
-		this.cod = cod;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getApe() {
-		return ape;
-	}
-	public void setApe(String ape) {
-		this.ape = ape;
-	}
 	
 }
